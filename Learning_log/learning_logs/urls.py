e2a2a480fe66cf9576b502cb974087429433f795
MoraXX-D <shubham.21dec03@gmail.com>
2024-04-24 19:09:37 +0530

    projects

--- Learning_log/learning_logs/urls.py
+++ Learning_log/learning_logs/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     # show all topics
     path('topics/', views.topics, name='topics'),
+    # detail page for single topic
+    path('topic/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic,name='new_topic'),
+
+
 ]
